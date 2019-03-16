8ff4d66dba59d926667fdc169330a37b81f9b5c1
Alex Bajzat <alex.bajzat@gmail.com>
2019-03-16 23:00:33 +0200

    styles on home page

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import style from './Header.module.css';
+import React from "react";
+import style from "./Header.module.css";
 
 export default function Header() {
-
-    return (
-        <div className={`${style.navbar} py-3 px-0 shadow-sm`}>
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-6 col-xl-4">
-                        <h2 className="mb-0">
-                            <a href="./" className={style.headerTitle}>Paul Photography</a>
-                        </h2>
-                    </div>
-                    <div className="col-6 col-xl-8 text-right">
-                        <nav className="text-right  d-lg-inline-block text-right">
-                            <ul className="mx-auto d-lg-block d-none mb-0">
-                                <li className={`${style.siteMenuEntry} px-3`}><a>Home</a></li>
-                                <li className={`${style.siteMenuEntry} px-3`}><a>Home</a></li>
-                                <li className={`${style.siteMenuEntry} px-3`}><a>Home</a></li>
-                                <li className={`${style.siteMenuEntry} px-3`}><a>Home</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={`${style.navbar} py-3 px-0 shadow-sm`}>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-6 col-xl-4">
+            <h2 className="mb-0">
+              <a href="./" className={style.headerTitle}>
+                Paul Photography
+              </a>
+            </h2>
+          </div>
+          <div className="col-6 col-xl-8 text-right">
+            <nav className="text-right  d-lg-inline-block text-right">
+              <ul className="mx-auto d-lg-block d-none mb-0">
+                <li className={`${style.siteMenuEntry} px-3`}>
+                  <a>Home</a>
+                </li>
+                <li className={`${style.siteMenuEntry} px-3`}>
+                  <a>Home</a>
+                </li>
+                <li className={`${style.siteMenuEntry} px-3`}>
+                  <a>Home</a>
+                </li>
+                <li className={`${style.siteMenuEntry} px-3`}>
+                  <a>Home</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Typing from "react-typing-animation";
 import style from "./Home.module.css";
 
 export default function Home() {
+  const typingMessagePart1 = "Your memories matter.";
+  const typingMessagePart2 = "I can help make them last";
   return (
-    <div className={style.main}>
-      <div className={`${style.bg} animated bounce slow`} />
-      <div className={`${style.profile} container`}>
-        <div className="row">
+    <div className={style.header}>
+      <div className={`${style.bg} `} />
+      
+      <div className={`${style.profile} `}>
+        <div className={style.profileImgWrapper}>
           <div className={`${style.profileImg} shadow`} />
+        </div>
+        <div className={`container text-center ${style.messageContainer}`}>
+          <Typing speed={100}>
+            <p>{typingMessagePart1}</p>
+            <Typing.Delay ms={800} />
+            <p>{typingMessagePart2}</p>
+          </Typing>
         </div>
       </div>
     </div>
