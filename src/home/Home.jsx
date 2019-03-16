d01a435c485b23fc111aae66f460579e8e8ba818
Alex Bajzat <alex.bajzat@gmail.com>
2019-03-16 23:09:35 +0200

    more fixes

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,21 +4,21 @@ import style from "./Home.module.css";
 
 export default function Home() {
   const typingMessagePart1 = "Your memories matter.";
-  const typingMessagePart2 = "I can help make them last";
+  const typingMessagePart2 = "I can help make them last.";
   return (
     <div className={style.header}>
       <div className={`${style.bg} `} />
-      
-      <div className={`${style.profile} `}>
-        <div className={style.profileImgWrapper}>
+
+      <div className={`${style.profileWrapper} container`}>
+        <div className={style.profile}>
           <div className={`${style.profileImg} shadow`} />
-        </div>
-        <div className={`container text-center ${style.messageContainer}`}>
-          <Typing speed={100}>
-            <p>{typingMessagePart1}</p>
-            <Typing.Delay ms={800} />
-            <p>{typingMessagePart2}</p>
-          </Typing>
+          <div className={`container text-center px-4 ${style.messageContainer}`}>
+            <Typing speed={100}>
+              <p>{typingMessagePart1}</p>
+              <Typing.Delay ms={800} />
+              <p>{typingMessagePart2}</p>
+            </Typing>
+          </div>
         </div>
       </div>
     </div>
