9319549c58c07c57f52737e922c9ff00d18c3c83
Alex Bajzat <alex.bajzat@gmail.com>
2019-03-19 23:29:22 +0200

    rearrangement

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,35 +7,35 @@ export default function Home() {
   const typingMessagePart2 = "I can help make them last. ";
 
   return (
-    <div className={`${style.header} shadow`}>
-      <div className={style.bg}>
-        <div className={style.bgOverlay} />
-      </div>
-
-      <div className={`${style.profileWrapper} container`}>
-        <div className={style.profile}>
-          <div className={`${style.profileImg} shadow`}>
-            <div className={style.profileImgOverlay}>
-              <div className={style.fbIcon} />
-              <div className={style.instaIcon} />
+    <div>
+      <div className={`${style.header} shadow`}>
+        <div className={style.bg}>
+          <div className={style.bgOverlay} />
+          <div className="container">
+            <div className={`${style.profileImg} shadow`}>
+              <div className={style.profileImgOverlay}>
+                <div className={style.fbIcon} />
+                <div className={style.instaIcon} />
+              </div>
             </div>
           </div>
-          <div
-            className={`container text-center px-4 ${style.messageContainer}`}
-          >
-            <Typing speed={90}>
-              <p>{typingMessagePart1}</p>
-              <Typing.Delay ms={500} />
-              <p>{typingMessagePart2}</p>
-            </Typing>
-          </div>
-          <div className={`container text-center ${style.separatorGrid}`}>
-            <div className={style.lineSeparator} />
-            <div className={style.cameraSeparator} />
-            <div className={style.lineSeparator} />
-          </div>
         </div>
-        <div className={style.homeContent} />
+      </div>
+
+      <div className={style.homeContent}>
+        <div className={`container text-center px-4 ${style.messageContainer}`}>
+          <Typing speed={90}>
+            <p>{typingMessagePart1}</p>
+            <Typing.Delay ms={500} />
+            <p>{typingMessagePart2}</p>
+          </Typing>
+        </div>
+        <div className={`container text-center ${style.separatorGrid}`}>
+          <div className={style.lineSeparator} />
+          <div className={style.cameraSeparator} />
+          <div className={style.lineSeparator} />
+        </div>
+        
       </div>
     </div>
   );
