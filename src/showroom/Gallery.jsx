2cd75f3083a97329f5ce020fafb10de25865608a
Alex Bajzat <alex.bajzat@gmail.com>
2019-06-08 19:51:51 +0300

    Added animation to grid

--- src/showroom/Gallery.jsx
+++ src/showroom/Gallery.jsx
@@ -8,14 +8,28 @@ export default function Gallery(props) {
 
     const self = useRef();
     const [galleryPreview, setGalleryPreview] = useState();
+
+    // some constants to manually manage page arrangements 
     const SCREEN_WIDTH_TRESHOLD = 600;
     const LARGE_SCREEN_WIDTH_TRESHOLD = 800;
     const MOBILE_IMAGE_MAX_WIDTH = 200;
     const DESKTOP_IMAGE_MAX_WIDTH = 300;
+    const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+
+    // TODO
+    var oldSelectedIdx;
     var selectedIdx;
+    var refsCollection = {};
+    var autoPlayIntervalID;
+    var availableIdxs = [];
 
     useEffect(() => {
         doImagePlacement();
+        availableIdxs = range(0, images.length, 1);
+
+        setTimeout(() => {
+            autoPlayIntervalID = setInterval(doRandomAutoplay, 3000);
+        }, 5000);
     }, [])
 
 
@@ -68,11 +82,47 @@ export default function Gallery(props) {
             key={idx}
             className={`${styles.image}`}
             style={randomWithStyle}
-            onClick={() => {onImageSelectedCallback(image)}}
-            >
+            ref={(inst) => refsCollection[idx] = {
+                instance: inst,
+                image: image
+            }}
+            onClick={() => {
+                doImageSelection(idx);
+            }}
+        >
             <div className={`${styles.previewClickArea}`}>
             </div>
         </div>;
+    }
+
+    function doImageSelection(idx) {
+        onImageSelectedCallback(refsCollection[idx].image);
+        markSelected(idx);
+        markImageAsVisited(idx);
+    }
+
+    function doRandomAutoplay() {
+        if (availableIdxs.length == 0) {
+            availableIdxs = range(0, images.length, 1);
+        }
+
+        const idx = Math.floor(Math.random() * availableIdxs.length);
+        markImageAsVisited(idx);
+        doImageSelection(idx);
+    }
+
+    function markImageAsVisited(idx) {
+        availableIdxs.splice(idx, 1);
+    }
+
+    function markSelected(idx) {
+        oldSelectedIdx = selectedIdx;
+        selectedIdx = idx;
+        if (oldSelectedIdx != null) {
+            const notSelectedClass = refsCollection[oldSelectedIdx].instance.className.replace(styles.imageSelected, "");
+            refsCollection[oldSelectedIdx].instance.className = notSelectedClass;
+        }
+        refsCollection[selectedIdx].instance.className += ` ${styles.imageSelected}`;
     }
 
     return (
